e35f1ce5d297371cfd07f419886b510d014f828e
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-02-26 01:26:30 +0530

    hh

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib import messages
 from .models import Customer,  Product, Cart, OrderPlaced
+from .forms import CustomerRegistrationForm
 
 class ProductView(View):
  def get (self, request): 
@@ -15,7 +17,7 @@ class ProductDetailView(View):
         return render(request,'app/productdetail.html',{'product':product})
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+ return render(request,'app/addtocart.html')
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
@@ -38,13 +40,26 @@ def Eyeeglasses(request, data=None):
         Eyeeglasses = Product.objects.filter(category='E')
     elif data == 'Dior' or data == 'Prada':
       Eyeeglasses = Product.objects.filter(category='E').filter(brand=data)
+    elif data =='below':
+      Eyeeglasses=Product.objects.filter(category='E').filter(discounted_price__lt=5000)
+    elif data =='above':
+      Eyeeglasses=Product.objects.filter(category='E').filter(discounted_price__gt=5000)
     return render(request,'app/Eyeeglasses.html',{'Eyeeglasses':Eyeeglasses})
 
 def login(request):
- return render(request, 'app/login.html')
+ return render(request, 'app/login.html') 
 
-def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+class CustomerRegistrationView(View):
+  def get(self,request):
+    form = CustomerRegistrationForm()
+    return render(request,'app/customerregistration.html',{'form':form})
+  
+def post(self,request):
+  form = CustomerRegistrationForm(request.POST)
+  if form.is_valid():
+    messages.success(request,'Congratulations!! registered succesfully')
+    form.save()
+  return render(request,'app/customerregistration.html',{'form':form}) 
 
 def checkout(request):
  return render(request, 'app/checkout.html')
