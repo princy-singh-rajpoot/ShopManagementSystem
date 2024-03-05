5f905c2e1c05181b4c4972b3f39013cf5b24536a
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-03-05 12:10:43 +0530

    k

--- app/models.py
+++ app/models.py
@@ -40,28 +40,28 @@ CATEGORY_CHOICES = (
 )
 
 class Product(models.Model):
-     title = models.CharField(max_length=100)
-     selling_price=models.FloatField()
-     discounted_price=models.FloatField()
-     description = models.TextField()
-     brand=models.CharField(max_length=100)
-     category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
-     product_image=models.ImageField(upload_to='productimg')
+ title = models.CharField(max_length=100)
+ selling_price=models.FloatField()
+ discounted_price=models.FloatField()
+ description = models.TextField()
+ brand=models.CharField(max_length=100)
+ category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+ product_image=models.ImageField(upload_to='productimg')
 
 def __Str__ (self):
-      return str (self.id)
+ return str (self.id)
 
 class Cart(models.Model):
-     user=models.ForeignKey(User,on_delete=models.CASCADE)
-     product=models.ForeignKey(Product,on_delete=models.CASCADE)
-     quantity=models.PositiveIntegerField(default=1)
+ user=models.ForeignKey(User,on_delete=models.CASCADE)
+ product=models.ForeignKey(Product,on_delete=models.CASCADE)
+ quantity=models.PositiveIntegerField(default=1)
 
-def __str__(self):
+ def __str__(self):
    return str(self.id)
 
-@property
-def total_cost(self):
-  return self.quantity * self.product.discounted_price
+ @property
+ def total_cost(self):
+   return self.quantity * self.product.discounted_price
     
 STATUS_CHOICES = (
      ('Accepted','Accepted'),
@@ -72,11 +72,15 @@ STATUS_CHOICES = (
 )
 
 class OrderPlaced(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
-    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
-    product=models.ForeignKey(Product,on_delete=models.CASCADE)
-    quantity=models.PositiveIntegerField(default=1)
-    ordered_date=models.DateTimeField(auto_now_add=True)
-    status=models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+ user= models.ForeignKey(User, on_delete=models.CASCADE)
+ customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+ product=models.ForeignKey(Product,on_delete=models.CASCADE)
+ quantity=models.PositiveIntegerField(default=1)
+ ordered_date=models.DateTimeField(auto_now_add=True)
+ status=models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+
+ @property
+ def total_cost(self):
+    return self.quantity * self.product.discounted_price
 
     
